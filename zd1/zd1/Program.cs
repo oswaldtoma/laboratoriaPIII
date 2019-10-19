7c715ac146d3a7956c98c9499440c485e4f77f73
warriortweety <oswald.toma@gmail.com>
2019-10-19 13:06:24 +0200

    commit branch

---
+++ zd1/zd1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zd1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("cos na ekranie");
+        }
+    }
+}
