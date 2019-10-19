b04bc78633b84cc2f0584a3627257959f298cdb4
warriortweety <oswald.toma@gmail.com>
2019-10-19 14:43:13 +0200

    zajecia 4

---
+++ zajecia4/zajecia4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace zajecia4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[][] tablica =
+            {
+                new[]{1, 2, 3, 4},
+                new[]{1, 2},
+                new[]{1},
+                new[]{1, 2, 3, 4, 5, 6, 7},
+            };
+
+            int[][] newTablica = new int[tablica.Length][];
+
+            bool state = false;
+            klasaKopiujaca.skopiujTablice(tablica, newTablica, ref state);
+            Console.WriteLine(state);
+        }
+    }
+}
