01607f24c3dbe9430084b7deb464ce5cd0a64b43
Oswald Toma <oswald.toma@gmail.com>
2019-11-05 20:37:31 +0100

    zadanie domowe 2 - wstepny commit

---
+++ zd2/zd2/CUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Xml.Schema;
+
+namespace zd2
+{
+    public class CUser
+    {
+        public string Username { get; }
+        public string Password { get; }
+        public int ItemsAmount { get; set; }
+
+        public CUser(){}
+        public CUser(string _u, string _p)
+        {
+            Username = _u;
+            Password = _p;
+        }
+
+        public void produceAnItem()
+        {
+            ItemsAmount++;
+            Console.WriteLine("To juz " + ItemsAmount + " w tej sesji");
+        }
+    }
+}
---
+++ zd2/zd2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace zd2
+{
+    public class Program
+    {
+        private static void logout()
+        {
+            //dodac zapis do pliku
+            Console.WriteLine("Do zobaczenia!");
+        }
+        public static void Main(string[] args)
+        {
+            CUser user = new CUser();
+            if (CAccountSystem.loginProcedure(ref user))
+            {
+                Console.WriteLine("Witaj, " + user.Username);
+                string userInput;
+                do
+                {
+                    Console.Write("(1). Wyprodukuj przedmiot \n" +
+                                  "(2). Sprawdz ilosc przedmiotow \n" +
+                                  "(x). Wyloguj \n");
+                    userInput = Console.ReadLine();
+
+                    switch (userInput)
+                    {
+                        case "1":
+                            user.produceAnItem();
+                            break;
+                        case "2":
+                            Console.WriteLine("Ilosc wyprodukowanych przed Ciebie przedmiotow wynosi: " + user.ItemsAmount);
+                            break;
+                        case "x":
+                            logout();
+                            break;
+                        default:
+                            Console.WriteLine("Nie ma takiej opcji...");
+                            break;
+                    }
+                } while (userInput != "x");
+            }
+        }
+    }
+}
