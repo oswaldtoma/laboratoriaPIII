a4e5311874f6228a8e3ec443e53d15f1e1052f4b
Oswald Toma <oswald.toma@gmail.com>
2019-12-02 16:45:12 +0100

    commit

---
+++ zd3/zd3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace zd3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            
+            CUczen jankowalski = new CUczen("Jan", "Kowalski");
+            
+            jankowalski.wystawOcene(COcena.TOceny.matematyka.ToString(), 3.0, 2);
+            jankowalski.wystawOcene(COcena.TOceny.matematyka.ToString(), 3.5, 5);
+            jankowalski.wystawOcene(COcena.TOceny.matematyka.ToString(), 2.0, 3);
+            jankowalski.wystawOcene(COcena.TOceny.matematyka.ToString(), 4.0, 1);
+            
+            Console.WriteLine(jankowalski.sredniaWazona());
+        }
+    }
+}
